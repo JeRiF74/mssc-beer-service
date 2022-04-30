d45325f2a42861900c95186e8691a7e7c1c676e3
JeRiF74 <jerif174@gmail.com>
2022-04-30 16:57:02 +0400

    Modeling new JPA domain, issue number #5

---
+++ src/test/java/az/micro/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,29 @@
+package az.micro.msscbeerservice.web.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.UUID;
+
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getBeerById() {
+
+    }
+
+    @Test
+    void saveNewBeer() {
+    }
+
+    @Test
+    void updateBeer() {
+    }
+}
